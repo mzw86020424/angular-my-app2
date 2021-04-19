6807445e268209119bfed0e81ad065a28a49c4d1
YutaMizusawa <yuta.mizusawa@dive.design>
2021-04-19 15:06:26 +0900

    ユーザー情報編集フォームを実装

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +22,8 @@ import { UserEditComponent } from './user/user-edit/user-edit.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [
     UserService
--- src/app/user/user-edit/user-edit.component.ts
+++ src/app/user/user-edit/user-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+
+import { User } from "../user";
+import { UserService } from "../user.service";
+
 
 @Component({
   selector: 'app-user-edit',
@@ -6,10 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-edit.component.css']
 })
 export class UserEditComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: UserService
+  ) { }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.user = this.service.getUser(id);
+  }
+
+  onSubmit(form: any): void {
+    let user = {
+      id: form.id,
+      name: form.name,
+      email: form.email
+    };
+    this.service.setUser(user);
+    this.router.navigate(["/users"]);
   }
 
 }
